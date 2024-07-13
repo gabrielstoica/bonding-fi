c74892f197b54e32fdfb958b1f7b1ec8b102a421
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-07-13 17:39:19 +0300

    feat: add 'LinearCurve' linear bounding curve contract

---
+++ src/LinearCurve.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import { UD60x18, pow, ud, unwrap } from "@prb/math/src/UD60x18.sol";
+import { ILinearCurve } from "./interfaces/ILinearCurve.sol";
+
+/// @title LinearCurve
+/// @notice See the documentation in {ILinearCurve}
+abstract contract LinearCurve is ILinearCurve {
+    /*//////////////////////////////////////////////////////////////////////////
+                                  PUBLIC STORAGE
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @inheritdoc ILinearCurve
+    UD60x18 public immutable override slope;
+
+    /// @inheritdoc ILinearCurve
+    UD60x18 public immutable override initialPrice;
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                    CONSTRUCTOR
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @dev Initializes the curve slope and the initial price.
+    constructor(UD60x18 _slope, UD60x18 _initialPrice) {
+        slope = _slope;
+        initialPrice = _initialPrice;
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                   PUBLIC METHODS
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @inheritdoc ILinearCurve
+    function getPrice(uint256 currentSupply, uint256 newSupply) public view returns (uint256 price) {
+        UD60x18 q1 = _calculateIntegralAtPoint(newSupply);
+        UD60x18 q0 = _calculateIntegralAtPoint(currentSupply);
+
+        price = unwrap(q1 - q0);
+    }
+
+    /// @inheritdoc ILinearCurve
+    function getCurrentPrice(uint256 currentSupply) public view returns (uint256 price) {
+        return unwrap(initialPrice.add(slope.mul(ud(currentSupply))));
+    }
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                   INTERNAL METHODS
+    //////////////////////////////////////////////////////////////////////////*/
+
+    /// @notice Calculates the integral at a specific point based on the linear curve formula
+    /// @dev The integral (I) is calculated as follows:
+    /// I(f(x) dx) = I(a + b*x) dx = I(a dx) + b*I(x dx) = a*x + (b*x^2)/2
+    function _calculateIntegralAtPoint(uint256 x) internal view returns (UD60x18) {
+        return initialPrice.mul(ud(x)).add(slope.mul(pow(ud(x), ud(2e18))).div(ud(2e18)));
+    }
+}
