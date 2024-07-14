6027f1499562597cb9767baa2e459c10b02437d7
gabrielstoica <marius_gabriel1998@yahoo.com>
2024-07-14 10:33:50 +0300

    test(linear-curve): add tests for 'buy' and 'getCurrentPrice' methods

--- test/Base.t.sol
+++ test/Base.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.20;
 
 import { Events } from "./utils/Events.sol";
 import { Users } from "./utils/Types.sol";
+import { ContinuousToken } from "./../src/ContinuousToken.sol";
 import "forge-std/Test.sol";
 
 abstract contract Base_Test is Test, Events {
@@ -16,17 +17,36 @@ abstract contract Base_Test is Test, Events {
                                    TEST CONTRACTS
     //////////////////////////////////////////////////////////////////////////*/
 
+    ContinuousToken internal continuousToken;
+
     /*//////////////////////////////////////////////////////////////////////////
                                   SET-UP FUNCTION
     //////////////////////////////////////////////////////////////////////////*/
 
     function setUp() public virtual {
-        users = Users({ admin: createUser("admin") });
+        users = Users({ admin: createUser("admin"), bob: createUser("bob"), eve: createUser("eve") });
+
+        continuousToken = deployContinousToken({
+            _name: "ContinuousLinearToken",
+            _symbol: "CLT",
+            _slope: 0.0005e18,
+            _initialPrice: 1e18
+        });
     }
 
     /*//////////////////////////////////////////////////////////////////////////
                             DEPLOYMENT-RELATED FUNCTIONS
     //////////////////////////////////////////////////////////////////////////*/
+
+    /// @dev Deploys a new {ContinuousToken} contract
+    function deployContinousToken(
+        string memory _name,
+        string memory _symbol,
+        uint256 _slope,
+        uint256 _initialPrice
+    ) internal returns (ContinuousToken) {
+        return new ContinuousToken(_name, _symbol, _slope, _initialPrice);
+    }
 
     /*//////////////////////////////////////////////////////////////////////////
                                     OTHER HELPERS
@@ -35,7 +55,7 @@ abstract contract Base_Test is Test, Events {
     /// @dev Generates a user, labels its address, and funds it with test assets
     function createUser(string memory name) internal returns (address payable) {
         address payable user = payable(makeAddr(name));
-        vm.deal({ account: user, newBalance: 100 ether });
+        vm.deal({ account: user, newBalance: 100_000 ether });
 
         return user;
     }
--- test/utils/Types.sol
+++ test/utils/Types.sol
@@ -4,4 +4,8 @@ pragma solidity ^0.8.20;
 struct Users {
     // Default admin of the Counter contract
     address payable admin;
+    // Bob's account
+    address payable bob;
+    // Eve's account
+    address payable eve;
 }
